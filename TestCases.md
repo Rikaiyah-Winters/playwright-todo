ae2163d30b35c9dabe781b6372b0d1c394aa7490
Rikaiyah Winters <rikaiyahswinters@gmail.com>
2026-03-03 09:58:30 -0600

    Added 3 more tests

--- TestCases.md
+++ TestCases.md
@@ -1,11 +1,23 @@
 Test Cases
 
-1.write a todo list item and make sure it's in 'active' ✅
-2.mark item out as completed Make sure completed item is in the 'completed' tab ✅
-4.unmark complete item so it's uncompleted again ✅
+Add:
+-- write a todo list item and make sure it's in 'active' ✅
 
-4.delete a todo list item
-5.go to 'active' page and ensure that all there are active
-6.go to 'completed' page and ensure that all there are completed
-7.clear completed
-8.check the little 'x item left' in the lower left side to make sure its accurate
+
+Filter:
+-- mark item out as completed Make sure completed item is in the 'completed' tab ✅
+-- go to 'completed' page and ensure that all there are completed 
+-- unmark complete item so it's active again ✅
+-- go to 'active' page and ensure that all there are active
+
+Delete:
+-- delete a todo list item ✅
+
+Edit:
+-- edit a test case✅
+
+-- clear completed ✅
+
+-- check the little 'x item left' in the lower left side to make sure its accurate 🟡
+
+-- local storage resistance test (whatever that means)
--- tests/todo.spec.ts
+++ tests/todo.spec.ts
@@ -52,6 +52,56 @@ test.describe("ToDo MVC UI Tests", () => {
         await expect(page.getByRole("listitem").filter({ hasText: "Pilates" })).not.toContainClass("completed")
     })
 
-    
-})
+    test("Deleted a test item", async ({ page }) => {
+        await page.getByTestId("text-input").fill("Pilates");
+        await page.getByTestId("text-input").press("Enter");
+        await expect(page.getByTestId("todo-item-label").filter({ hasText: "Pilates" })).toBeVisible();
 
+        await page.getByRole("listitem").filter({ hasText: "Pilates" }).getByTestId('todo-item-button').click(); //idk why this step takes forever. need to find out why its flaky
+        await expect(page.getByTestId("todo-item-label").filter({ hasText: "Pilates" })).not.toBeVisible();
+    })
+
+    test("Edit/Update a list item", async ({ page }) => {
+        //this is where double click comes in!!!
+        await page.getByTestId("text-input").fill("Pilates");
+        await page.getByTestId("text-input").press("Enter"); //this is also a good fixture or put in the before each part
+        await page.getByTestId('todo-item-label').filter({ hasText: "Pilates" }).dblclick();
+        await page.getByTestId('todo-item').getByTestId('text-input').fill('edit this something new');
+        await page.getByTestId('todo-item').getByTestId('text-input').press('Enter');
+    })
+
+    test("Clear completed items", async ({ page }) => {
+        await page.getByTestId("text-input").fill("one");
+        await page.getByTestId("text-input").press("Enter");
+        await page.getByTestId("text-input").fill("two");
+        await page.getByTestId("text-input").press("Enter");
+        await page.getByTestId("text-input").fill("three");
+        await page.getByTestId("text-input").press("Enter");
+
+        await page.getByRole("listitem").filter({ hasText: "two" }).getByTestId("todo-item-toggle").click();
+
+        //make sure that it has the complete class on all tab
+        await expect(page.getByRole("listitem").filter({ hasText: "two" })).toHaveClass("completed");
+        //make sure that it is located in the complete tab with the completed class
+        await page.getByRole("link", { name: "Completed" }).click()
+        await expect(page.getByRole("listitem").filter({ hasText: "two" })).toHaveClass("completed");
+        //click the clear completed button
+        await page.getByRole("button", { name: "Clear completed" }).click()
+        //make sure that it is not located in either the all or completed tabs
+        await page.getByRole("link", { name: "All" }).click()
+        await expect(page.getByRole("listitem").filter({ hasText: "two" })).not.toBeVisible();
+        await page.getByRole("link", { name: "Completed" }).click()
+        await expect(page.getByRole("listitem").filter({ hasText: "two" })).not.toBeVisible();
+    })
+
+    test("Ensure 'todo-count' matches the amount of active list items", async ({ page }) => {
+        await page.getByTestId("text-input").fill("one");
+        await page.getByTestId("text-input").press("Enter");
+        await page.getByTestId("text-input").fill("two");
+        await page.getByTestId("text-input").press("Enter");
+        await page.getByTestId("text-input").fill("three");
+        await page.getByTestId("text-input").press("Enter");
+        await expect(page.getByTestId("todo-list").getByRole("listitem")).toHaveCount(3);
+        //would be nice to match the li count to the actual span number in the todo counter
+    })
+});
